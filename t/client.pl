2b42dd7e02ee8709ef4865e1491b2c56422ceaa6
Lenz Gschwendtner <lenz@springtimesoft.com>
2009-09-25 12:00:26 +1200

    very primitive test client

---
+++ t/client.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/perl -Ilib
+
+use strict;
+use warnings;
+use Net::AMQP::Simple;
+
+my $amqp = Net::AMQP::Simple->new();
+
+$amqp->connect();
+#print $amqp->close_channel();
+print $amqp->pub("helo world", "log");
+
